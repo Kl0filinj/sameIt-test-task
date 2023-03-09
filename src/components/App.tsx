5bcf8ea7706652d5bc5cb9283f2e25bc7f843e64
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-03-09 12:44:33 +0100

    set up Mui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,5 @@
 import React from 'react';
 
 export const App: React.FC = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div></div>;
 };
